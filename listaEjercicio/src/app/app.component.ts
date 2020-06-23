4f68513a5853e038907300ab82ceb832d706f982
blueC4P <osamag4g@gmail.com>
2020-06-24 00:28:52 +0100

    fechayhoraFunction

--- listaEjercicio/src/app/app.component.ts
+++ listaEjercicio/src/app/app.component.ts
@@ -6,9 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tareas: string[] = [];
-  tarea: string = "";
-  
+  tareas: string[] = []; //tipamos el array de strings e inicializamos
+  tarea: string = "";     //tipamos como string e inicializamos
+  hoy = new Date();       //creamos una variable donde guardamos los datos
+
+  fechaFunction(){
+    // creamos fecha actual
+    let fecha = this.hoy.getDate() + '/' + (this.hoy.getMonth()+1) + '/' + this.hoy.getFullYear();
+    console.log(fecha);
+    // creamos la hora actual
+    let hora = this.hoy.getHours() + ':' + this.hoy.getMinutes() + ':' + this.hoy.getSeconds();
+    console.log(hora);
+    return ` Fecha: ${fecha} y hora: ${hora} `;
+  }
+
   addActivity(){
 
     // funcion para agregar cosas al array
